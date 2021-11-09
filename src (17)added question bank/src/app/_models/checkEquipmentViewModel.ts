3716f2eff8a2688ce62daafdf7aa0715ec3e04a4
Joyous <62016480+JoyousKonyana@users.noreply.github.com>
2021-11-09 13:35:47 +0200

    added view model

--- src (17)added question bank/src/app/_models/checkEquipmentViewModel.ts
+++ src (17)added question bank/src/app/_models/checkEquipmentViewModel.ts
@@ -1,11 +1,12 @@
+import { Datetime } from "ng-modules";
+
 export class checkEquipmentViewModel {
-    // public int EquipmentId { get; set; }
-    //     public int OnboarderId { get; set; }
-    //     //public DateTime EquipmentCheckOutDate { get; set; }
+     EquipmentId!:number; 
+       OnboarderId:number;
+        //public DateTime EquipmentCheckOutDate { get; set; }
 
-    //     //public string EquipmentCheckOutCondition { get; set; }
+        //public string EquipmentCheckOutCondition { get; set; }
 
-    //     public DateTime EquipmentCheckInDate { get; set; }
-
-    //     public string EquipmentCheckInCondition { get; set; }
+         EquipmentCheckInDate:Datetime;
+         EquipmentCheckInCondition:Datetime 
 }
--- src (17)added question bank/src/app/equipment/my_equipment.component.ts
+++ src (17)added question bank/src/app/equipment/my_equipment.component.ts
@@ -8,6 +8,8 @@ import { first } from 'rxjs/operators';
 import { AssignEquipment, Equipment_Query, EquipmentQuery } from '../_models';
 import { EquipmentService, Equipment_QueryService, AlertService } from '../_services';
 import { stringify } from 'querystring';
+import { checkEquipmentViewModel } from '../_models/checkEquipmentViewModel';
+import { Datetime } from 'ng-modules';
 
 @Component({ 
     templateUrl: 'my_equipment.component.html',
@@ -155,6 +157,12 @@ equipid:number;
       EquipmentCheckInDate: date,
       EquipmentCheckInCondition: equipment.equipmentCheckInCondition
     };
+    const data2:checkEquipmentViewModel ={
+      EquipmentId: data.EquipmentId,
+      OnboarderId: data.OnboarderId,
+      EquipmentCheckInDate: new daye,
+      EquipmentCheckInCondition: new Datetime
+    }
     this.yService.checkEquipment(data)
       // .pipe(first())
       .subscribe(res => {
